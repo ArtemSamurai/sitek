10837bb3d9288bb0574609415d25b1c7b22ec464
ArtemSamurai <modealohadance9@gmail.com>
2025-03-20 19:35:02 +0200

    Initial commit

--- aiogram_pipi.py
+++ aiogram_pipi.py
@@ -1,14 +1,13 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types.web_app_info import WebAppInfo
 
-bot = Bot('7721361070:AAHMO6LC2EjtmpqsifsSkcMU4fgtDNErtvc')
+bot = Bot('7721361070:AAFSBMU41Alz2423lGH1RznrBnUvpzJGKls')
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def kaka(message: types.Message):
     kaka = types.ReplyKeyboardMarkup()
-    file = open('D:\python\Telegram_bot\papich maniakich.jpg')
-    kaka.add(types.KeyboardButton('Открыть веб-страницу', web_app=WebAppInfo(url='https://itproger.com/')))
+    kaka.add(types.KeyboardButton('Открыть веб-страницу', web_app=WebAppInfo(url='https://artemsamurai.github.io/sitek/')))
     await message.answer('Привет и иди нахуй))', reply_markup=kaka)
 
 executor.start_polling(dp)
